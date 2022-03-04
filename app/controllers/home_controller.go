8dedf84b2aa2bd8024a8a56bf36c5ccd89f6f24f
zumardirahman <zumar2014@gmail.com>
2022-03-04 12:08:59 +0700

    setup render html

--- app/controllers/home_controller.go
+++ app/controllers/home_controller.go
@@ -1,10 +1,20 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/unrolled/render"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to Golang Toko HOME")
+
+	render := render.New(render.Options{
+		Layout: "layout",
+	})
+
+	//module ini akan merender yang ada didalam module templates
+	_ = render.HTML(w, http.StatusOK, "home", map[string]interface{}{
+		"title": "Home Title",
+		"body":  "Home Description",
+	})
 }
