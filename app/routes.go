681cf0d1cd5dfc27a7eb9286917b4a76ba8e288f
zumardirahman <zumar2014@gmail.com>
2022-03-04 12:17:43 +0700

    setup file assets css

--- app/routes.go
+++ app/routes.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/zumardirahman/golang-toko/app/controllers"
 )
@@ -8,4 +10,10 @@ import (
 func (server *Server) initializeRoutes() {
 	server.Router = mux.NewRouter()
 	server.Router.HandleFunc("/", controllers.Home).Methods("GET") //routing tes pertama
+
+	//setup public directory
+	staticFileDirectory := http.Dir("./assets/")
+	staticFileHandler := http.StripPrefix("/public/", http.FileServer(staticFileDirectory))
+	server.Router.PathPrefix("/public").Handler(staticFileHandler).Methods("GET")
+
 }
